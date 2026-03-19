4ea323be856d857f7aef788c3d6e69b25accf247
Dikewonsi <91637528+Dikewonsi@users.noreply.github.com>
2026-03-19 15:12:57 +0100

    Update posts.js
    CRUD API setup complete

--- routes/posts.js
+++ routes/posts.js
@@ -50,4 +50,34 @@ router.post('/', (req, res) => {
     res.status(201).json(posts);
 });
 
+// Update Post
+router.put('/:id', (req, res) => {
+    const id = parseInt(req.params.id);
+    const post = posts.find((post) => post.id === id);
+
+    if(!post) {
+        return res
+        .status(404)
+        .json({msg: `No Post Found`});
+    }
+
+    post.title = req.body.title;
+    res.status(200).json(posts);
+});
+
+// Delete Post
+router.delete('/:id', (req, res) => {
+    const id = parseInt(req.params.id);
+    const post = posts.find((post) => post.id === id);
+
+    if(!post) {
+        return res
+        .status(404)
+        .json({msg: 'No Post Found.'});
+    }
+
+    posts = posts.filter((post) => post.id !== id);
+    res.status(200).json(posts);
+});
+
 export default router;
