dc926cc7ffec7b002b37c379414252c5c3bcfab0
Dikewonsi <91637528+Dikewonsi@users.noreply.github.com>
2026-03-23 13:37:41 +0100

    Update logger.js
    Add colors to logs. With express I have access to the complete req, and res cycle.

--- middleware/logger.js
+++ middleware/logger.js
@@ -1,5 +1,20 @@
+import colors from 'colors';
+
 const logger = (req, res, next) => {
-    console.log(`${req.method} ${req.protocol}://${req.get('host')}${req.originalUrl}`);
+    const methodColors = {
+        GET: 'green',
+        POST: 'blue',
+        PUT: 'yellow',
+        DELETE: 'red'
+    };
+
+    const color = methodColors[req.method] || white;
+
+    const message = `${req.method} ${req.protocol}://${req.get('host')}${req.originalUrl}`;
+
+    console.log(
+        message[color]
+    );
     next();
 };
 
